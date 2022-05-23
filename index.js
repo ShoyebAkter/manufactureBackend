743d8fefba0f0d540998e80e6072eaa73bc434b1
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-05-23 18:57:09 +0600

    problem in order api

--- index.js
+++ index.js
@@ -20,7 +20,8 @@ async function run(){
   try{
     await client.connect();
     const serviceCollection=client.db('computerhub').collection('tools');
-
+    const userCollection = client.db('computerhub').collection('users');
+    const orderCollection = client.db('computerhub').collection('order');
     app.get('/service',async(req,res)=>{
       const query={};
       const cursor=serviceCollection.find(query);
@@ -28,11 +29,60 @@ async function run(){
       res.send(result);
     })
 
+    app.put('/user/:email',async(req,res)=>{
+      const email=req.params.email;
+      const user=req.body;
+      const filter={email:email};
+      const options={upsert:true};
+      const updateDoc={
+        $set:user,
+      }
+      const result=await userCollection.updateOne(filter,updateDoc,options);
+      const token = jwt.sign({ email: email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+      res.send({ result, token });
+    })
+
     app.get('/service/:id',async(req,res)=>{
       const id=req.params.id;
       const query={_id:ObjectId(id)};
       const service=await serviceCollection.findOne(query);
       res.send(service);
+    })
+
+    app.put('/service/:id',async(req,res)=>{
+      const id=req.params.id
+      const updatedProduct=req.body
+      const filter ={ _id:ObjectId(id)}
+      const options = { upsert: true }
+      const updatedDoc={
+        $set:{
+          quantity: updatedProduct.quantity
+        }
+      }
+      const result=await serviceCollection.updateOne(filter,updatedDoc,options)
+      res.send(result)
+    })
+
+    //order
+
+    app.put('/service/:id',async(req,res)=>{
+      const id=req.params.id
+      const updatedProduct=req.body
+      const filter ={ _id:ObjectId(id)}
+      const options = { upsert: true }
+      const updatedDoc={
+        $set:{
+          quantity: updatedProduct.quantity
+        }
+      }
+      const result=await serviceCollection.updateOne(filter,updatedDoc,options)
+      res.send(result)
+    })
+
+    app.post('/order',async(req,res)=>{
+      const order=req.body;
+      const result=await orderCollection.insertOne(order);
+      res.send(result);
     })
 
   }finally{
