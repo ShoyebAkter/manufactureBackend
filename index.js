d8a374b5907d660c0e6f6d97d9c52c219e3a9cd4
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-05-23 08:44:28 +0600

    service api created

--- index.js
+++ index.js
@@ -16,7 +16,23 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.yba5w.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+  try{
+    await client.connect();
+    const serviceCollection=client.db('computerhub').collection('tools');
 
+    app.get('/service',async(req,res)=>{
+      const query={};
+      const cursor=serviceCollection.find(query);
+      const result=await cursor.toArray();
+      res.send(result);
+    })
+
+  }finally{
+
+  }
+}
+run().catch(console.dir)
 
 
 
