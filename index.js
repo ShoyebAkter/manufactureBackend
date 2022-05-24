711367678f6aeb22a78ec7baa4ce264398196046
Shoyeb180 <shoyebmohammad660@gmail.com>
2022-05-24 23:53:20 +0600

    server crash problem

--- index.js
+++ index.js
@@ -66,6 +66,13 @@ async function run(){
       res.send(result);
     })
 
+    app.delete('/service/:id',verifyJWT,verifyAdmin,async(req,res)=>{
+      const id=req.params.id;
+      const filter={_id:ObjectId(id)};
+      const result=await serviceCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/user',verifyJWT, async (req, res) => {
       const users = await userCollection.find().toArray();
       res.send(users);
